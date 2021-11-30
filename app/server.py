f1e95f118aa6a5c9bd46a97033a8937116ae7e39
raheem paxton <raheem.jp@gmail.com>
2021-11-30 14:56:33 -0600

    ML completed Flask completed

--- app/server.py
+++ app/server.py
@@ -1,14 +1,15 @@
 #import libraries
 import numpy as np
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import pickle
+import sklearn
 
 app = Flask(__name__)
 
 # prediction function
 def ValuePredictor(to_predict_list):
     to_predict = np.array(to_predict_list).reshape(1, 8)
-    loaded_model = pickle.load(open("model.pkl", "rb"))
+    loaded_model = pickle.load(open("finalized_model.pkl", "rb"))
     result = loaded_model.predict(to_predict)
     return result[0]
  
@@ -23,11 +24,14 @@ def result():
         to_predict_list = list(to_predict_list.values())
         to_predict_list = list(map(int, to_predict_list))
         result = ValuePredictor(to_predict_list)       
-        if int(result)== 1:
-            prediction ='He or She will likely GRADUATE or COMPLETE the program'
+        if result== 'Terminated/Discharge':
+            prediction ='He or She will likely be TERMINATED from the program'
         else:
-            prediction ='He or She will likely be TERMINATED from the program'                    
-        return render_template("predict.html", prediction = prediction)
+            prediction ='He or She will likely GRADUATE or COMPLETE the program'
+                                
+        return render_template("predict.html", 
+        prediction = prediction,
+        info = to_predict_list)
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
